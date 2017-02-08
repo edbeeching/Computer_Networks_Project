1d03204d216e19ddc69ebf04f5c961e9d074ddba
druaGa <Sejal@wifiroam074045.univ-st-etienne.fr>
2017-02-08 13:03:32 +0100

    Testing GUI elements

--- torrentNchill/GUI.py
+++ torrentNchill/GUI.py
@@ -1,1 +1,54 @@
 
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+
+Creating a skeleton of a basic GUI.
+This is to only test if the libraries work.
+Difficulty: using PyQt5 on Mac.
+
+Created on Mon Feb  6 22:53:25 2017
+@author: Sejal
+Last updated: Tue Feb 7
+
+"""
+
+import sys
+from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, QApplication
+from PyQt5.QtGui import QIcon
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        textEdit = QTextEdit()
+        self.setCentralWidget(textEdit)
+
+        exitAction = QAction(QIcon('exit24.png'), 'Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(self.close)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+        toolbar = self.addToolBar('Exit')
+        toolbar.addAction(exitAction)
+
+        #self.setGeometry(300, 300, 350, 250)
+        self.setWindowTitle('Main windows')
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
