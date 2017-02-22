b1f8bfcbf8c7ff684fff6de4c3b98ef35885b40b
druaga <cj.sejal@gmail.com>
2017-02-22 11:20:46 +0100

    First version of the file handler.

--- torrentNchill/file_handler.py
+++ torrentNchill/file_handler.py
@@ -33,7 +33,7 @@ class FileHandler(Thread):
         self.in_queue = in_queue
         self.out_queue = out_queue
         #Use later to fetch from Memory rather than the Disc
-        self.buffer_size = 1024
+        #self.buffer_size = 1024
         FileHandler.read_in()
 
     def write_part(composition_name, bytes_per_part, part, data):
